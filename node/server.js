7c982a5d885925ce14e14156a67a070ef97458b1
Melo Ortega <melo.dev@adabae.xyz>
2021-05-12 11:16:34 +0100

    testing branch

--- node/server.js
+++ node/server.js
@@ -3,11 +3,11 @@ var app = express();
 
 const port = 3000;
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+//Inicio del server
 server.listen(port, () => {
-  console.log(`El servidor se está ejecutando en ${port}`);
+  console.log(`Running.. ${port}`);
 });
