0a2ed80aa6d3bac78e037bec954ed3654d13a597
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-15 11:45:30 +0100

    Leer fichero json desde server.js

--- node/server.js
+++ node/server.js
@@ -1,10 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
-
 const port = 3000;
 
 app.get('/geox/shoes', (req, res) => {
-  res.send('');
+  let readFile = fs.readFileSync('shoesgeox.json');
+  res.status(200).json(readFile);
 });
 
 //Inicio del server
